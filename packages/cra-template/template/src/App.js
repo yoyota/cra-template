a05aa6893446d30a375573b4ae1669e6227466ea
yoyota <smarthorse6@gmail.com>
2020-02-16 15:38:52 +0900

    Add api example (#2)
    * Add api example
    
    * Delete console.log

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -1,11 +1,14 @@
 import React from "react"
 import { BrowserRouter as Router, Switch } from "react-router-dom"
 import { Container } from "react-bootstrap"
+import { ToastContainer } from "react-toastify"
+import "react-toastify/dist/ReactToastify.css"
 import routes from "./routes"
 
 function App() {
   return (
     <Router>
+      <ToastContainer />
       <Container>
         <Switch>{routes}</Switch>
       </Container>
