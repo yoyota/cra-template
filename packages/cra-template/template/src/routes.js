9a1292c401bc6f8cec139f8a39bf61e207f6bded
yoyota <smarthorse6@gmail.com>
2020-02-17 13:28:37 +0900

    Add apiExample test case

--- packages/cra-template/template/src/routes.js
+++ packages/cra-template/template/src/routes.js
@@ -2,7 +2,7 @@ import React from "react"
 import { Route } from "react-router-dom"
 import Home from "./components/Home"
 import Count from "./count"
-import ApiExample from "./components/ApiExample"
+import ApiExample from "./apiExample"
 
 const routes = [
   { exact: true, path: "/", component: Home },
