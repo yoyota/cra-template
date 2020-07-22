b39df249b2fb810a8f6382b2f07037f525c80f0d
Jeffrey Posnick <jeffy@google.com>
2020-07-21 22:27:32 -0400

    Switch to the Workbox InjectManifest plugin (#9205)
    Co-authored-by: Ian Schmitz <ianschmitz@gmail.com>

--- packages/cra-template/template/src/index.js
+++ packages/cra-template/template/src/index.js
@@ -2,8 +2,8 @@ import React from "react"
 import ReactDOM from "react-dom"
 import "typeface-roboto"
 import App from "./App"
-import * as serviceWorker from "./serviceWorker"
 import ErrorBoundary from "./ErrorBoundary"
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 import reportWebVitals from './reportWebVitals';
 
 const Root = () => (
@@ -19,7 +19,7 @@ ReactDOM.render(<Root />, document.getElementById("root"))
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://cra.link/PWA
-serviceWorker.unregister();
+serviceWorkerRegistration.unregister();
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
