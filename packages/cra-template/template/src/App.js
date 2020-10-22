55a27c51d02d27deef0e315ef5c11700c8182701
Brody McKee <mrmckeb@users.noreply.github.com>
2020-10-23 01:30:37 +0300

    feat: remove unused React imports (#9853)

--- packages/cra-template/template/src/App.js
+++ packages/cra-template/template/src/App.js
@@ -1,4 +1,3 @@
-import React from "react"
 import "react-toastify/dist/ReactToastify.css"
 import { Container, CssBaseline } from "@material-ui/core"
 import { ToastContainer } from "react-toastify"
